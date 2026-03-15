6f8333d0c99bf1949d26f4af8b3a08ca75ab986f
Jakub Sadrakuła <kuba@sadrakula.com>
2026-03-15 14:12:07 +0100

    Fix CI triggers and exclude build folders

--- build/CMakeFiles/fc-tmp/httplib/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for httplib")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/Users/kuba/2D-Racing-Game/build/CMakeFiles/fc-tmp/httplib/httplib-gitupdate.cmake")
-
-endblock()
